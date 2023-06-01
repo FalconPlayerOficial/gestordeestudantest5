643c4a2acbf3d9e7649879a3dd71bc5ee5339b45
FalconPlayerOficial <118479795+FalconPlayerOficial@users.noreply.github.com>
2023-06-01 18:07:12 -0300

    Update MEU_BD.cs

--- studant-menager/studant-menager/MEU_BD.cs
+++ studant-menager/studant-menager/MEU_BD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace studant_menager
 {
+
     internal class MEU_BD
     {
-    }
-}
+        private MySqlConnection conexao = null MySqlConnection("datasource=localhost;username=root;password=;database=t4_sga_bd");
+
+        private MySqlDataReader getConexao
+        {
+            get
+            {
+                return conexao;
+            }
+        }
+        public void abrirConexao()
+        {
+            if ( conexao.State == Connectionstate.Open)
+        }
+    }   
