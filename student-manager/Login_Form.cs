d89e5cc4f9a316a2601b0e4fb1f5aec73b622295
FalconPlayerOficial <118479795+FalconPlayerOficial@users.noreply.github.com>
2023-09-28 16:19:59 -0300

    update

---
+++ student-manager/Login_Form.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student_manager
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Image = Image.FromFile("../../imagens/user.png");
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            MEU_BD bancoDeDados = new MEU_BD();
+
+            MySqlDataAdapter adaptador = new MySqlDataAdapter();
+            DataTable tabela = new DataTable();
+            MySqlCommand comando = new MySqlCommand("SELECT * FROM `usuarios` WHERE `nome_de_usuario` = @usn AND `senha` = @psw", bancoDeDados.getConexao);
+
+           
+            comando.Parameters.Add("@usn", MySqlDbType.VarChar).Value = txtUsuario.Text;
+            comando.Parameters.Add("@psw", MySqlDbType.VarChar).Value = txtSenha.Text;
+
+            adaptador.SelectCommand = comando;
+
+            adaptador.Fill(tabela);
+
+            if (tabela.Rows.Count > 0)
+            {
+                
+                this.DialogResult = DialogResult.OK;
+            }
+            else 
+            {
+                MessageBox.Show("Usuário ou senha inválidos", 
+                    "Erro de Login", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ student-manager/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace student_manager
+{
+    internal static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Login_Form telaDeLogin = new Login_Form();
+
+            if (telaDeLogin.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new FormPrincipal());
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
